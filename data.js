7e2a36cdc0db41793c72ddc8605a5aa6a0f7a2f0
Florian Lohoff <flo@filoo.de>
2016-06-24 21:23:25 +0200

    Remove data from js file

---
+++ data.js
@@ -0,0 +1,28 @@
+
+var data=
+[
+{ lat: 51.9107, lon: 8.38387, street: 'Berliner Straße', type: 581 },
+{ lat: 51.911, lon: 8.385, street: 'Berliner Straße', type: 799 },
+{ lat: 51.9101, lon: 8.38167, street: 'Beriner Straße', type: 141 },
+];
+
+/*
+L.marker([51.9107, 8.38387]).addTo(mymap)
+.bindPopup("207 Typ: 581<br>UB 01: 21<br>U1 01: 45<br>UB 02: 71<br>U1 02: ").openPopup();
+L.marker([51.911, 8.385]).addTo(mymap)
+.bindPopup("58 Typ: 799<br>UB 01: 81<br>U1 01: 64<br>UB 02: 71<br>U1 02: ").openPopup();
+L.marker([51.9101, 8.38167]).addTo(mymap)
+.bindPopup("3 Typ: 141<br>UB 01: 71<br>U1 01: 49<br>UB 02: 21<br>U1 02: ").openPopup();
+
+
+L.marker([51.9129, 8.3791]).addTo(mymap)
+.bindPopup("54 Typ: 141<br>UB 01: 71<br>U1 01: 10<br>UB 02: 21<br>U1 02: ").openPopup();
+L.marker([51.9124, 8.37919]).addTo(mymap)
+.bindPopup("102 Typ: 799<br>UB 01: 21<br>U1 01: 10<br>UB 02: 71<br>U1 02: ").openPopup();
+L.marker([51.9116, 8.37972]).addTo(mymap)
+.bindPopup("41 Typ: 181<br>UB 01: 71<br>U1 01: 13<br>UB 02: <br>U1 02: ").openPopup();
+L.marker([51.9105, 8.3804]).addTo(mymap)
+.bindPopup("109 Typ: 371<br>UB 01: 71<br>U1 01: 14<br>UB 02: 21<br>U1 02: ").openPopup();
+L.marker([51.9097, 8.38117]).addTo(mymap)
+.bindPopup("161 Typ: 222<br>UB 01: 71<br>U1 01: 31<br>UB 02: 21<br>U1 02: ").openPopup();
+*/
